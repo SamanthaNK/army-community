5867da60d1e992ad14873537af1d397fbe7b3dbf
Samantha Ngong <samantha.ngong@student.kdg.be>
2025-05-13 16:29:37 +0200

    Initialize project with Gradle setup and Spring Boot application

---
+++ army-community/src/main/java/com/armycommunity/army_community/ArmyCommunityApplication.java
@@ -0,0 +1,12 @@
+package com.armycommunity.army_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmyCommunityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArmyCommunityApplication.class, args);
+	}
+
+}
---
+++ army-community/src/test/java/com/armycommunity/army_community/ArmyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armycommunity.army_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmyCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
